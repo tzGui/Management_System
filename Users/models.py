fed27ca278d3f7ac08bbbd8a1431705dcc3d0def
tzGui <devguithz@gmail.com>
2024-07-31 19:20:00 -0300

    Conclusão do cadastro(cargos) e função manter conectado

--- Users/models.py
+++ Users/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-from datetime import date
+from django.contrib.auth.models import User
 
+# Modelo de perfil de usuário que armazena o cargo
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    cargo = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.user.username
+
+# Signals para criar e salvar automaticamente o perfil do usuário
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    # Cria um perfil de usuário sempre que um novo usuário é criado
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    # Salva o perfil do usuário sempre que o usuário é salvo
+    instance.userprofile.save()
--- Users/views.py
+++ Users/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as login_django
 from django.contrib.auth.decorators import login_required
+from .models import UserProfile
 
 def cadastro(request):
     if request.method == "GET":
@@ -12,6 +13,7 @@ def cadastro(request):
         username = request.POST.get('username')
         email = request.POST.get('email')
         senha = request.POST.get('senha')
+        cargo = request.POST.get('cargo')
 
         user = User.objects.filter(username=username).first()
 
@@ -21,20 +23,34 @@ def cadastro(request):
         user = User.objects.create_user(username=username, email=email, password=senha)
         user.save()
 
+        user_profile = UserProfile(user=user, cargo=cargo)
+        user_profile.save()
+
         return HttpResponse('Usuário cadastrado com sucesso')
     
 
 def login(request):
     if request.method == "GET":
         return render (request, 'Users/login.html')
+    
+
     else:
         username = request.POST.get('username')
         senha = request.POST.get('senha')
 
         user = authenticate(username=username, password=senha)
 
+
         if user:
             login_django(request, user)
+
+            remember_me = request.POST.get('remember_me',False)
+
+            if not remember_me:
+                request.session.set_expiry(0)
+            else:
+                request.session.set_expiry(1209600)
+
             return HttpResponse('autenticado')
         else:
             return HttpResponse('email ou senha inválidos')
